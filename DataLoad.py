fa8709da7807dc58372796ab2b94ba8db63251f9
JukNavozn1k <deffender1337@gmail.com>
2023-04-19 02:11:30 +0300

    UPD gitignore | Added save,load (from file) functions

--- DataLoad.py
+++ DataLoad.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
+import json
 
 
 wordlist = dict()
@@ -9,7 +9,7 @@ wordlist = dict()
 session = requests.Session()
 
 
-def downloadTable(url, title):
+def downloadTable(url, title,out=0):
     wordlist[title] = []
     response = session.get(url)
     html = response.content
@@ -21,19 +21,28 @@ def downloadTable(url, title):
         for wordObject in wordObjects:
             word.append(re.sub(r'\xa0', '', wordObject.text))
         wordlist[title].append(word)
-        #print(word)
-    #print('-----------------------------------------------------------------------------')
+        if out != 0: print(word)
+    if out != 0: print('-----------------------------------------------------------------------------')
     
 
-def downloadWords(url):
+def downloadWords(url,out=0):
     response = session.get(url)
     html = response.content
     soup = BeautifulSoup(html, 'html.parser')
     lis = soup.findAll('li')
     for link in lis[23:99]:
         hz = link.find('a', href=True).text
-        downloadTable(link.find('a', href=True)['href'], link.find('a', href=True).text)
+        downloadTable(link.find('a', href=True)['href'], link.find('a', href=True).text,out)
 
+def get_words(out=0):
 
-downloadWords('https://langformula.ru/voc3000/')
-print(wordlist)
+    downloadWords('https://langformula.ru/voc3000/',out=1)
+    #print(wordlist)
+
+    with open("words.json", "w") as outfile:
+        json.dump(wordlist, outfile)
+def load_words():
+    with open('words.json', 'r') as infile:
+        my_dict = json.load(infile)
+    return my_dict
+
